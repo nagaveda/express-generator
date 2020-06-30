2ec4b1e9376fb71c3d769b1932066e9a8205f545
nagaveda <gajjalanagaveda@karunya.edu.in>
2020-06-30 10:20:14 +0530

    Oauth-passport facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     "secretKey": "12345-67890-09876-54321",
-    "mongoUrl": "mongodb://localhost:27017/conFusion" 
+    "mongoUrl": "mongodb://localhost:27017/conFusion" ,
+    "facebook": {
+        "clientId": '290515772144565',
+        "clientSecret":'e4b9de5afaf2cde92879466e560a09e0'
+    }
 }
